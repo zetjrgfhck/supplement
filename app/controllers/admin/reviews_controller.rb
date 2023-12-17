61bb238c31b9571348ec33cbd40f46def971b54d
HIDEKAZU SHINOHARA <141484383+zetjrgfhck@users.noreply.github.com>
2023-12-17 15:59:04 +0000

    Initial Commit

--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -1,7 +1,18 @@
 class Admin::ReviewsController < ApplicationController
 
   def index
-    @reviews = Review.all
+    keyword = params[:keyword]
+    order_by = params[:order_by] # ASC or DESC
+    if order_by == "ASC"
+      @reviews = Review.where("title LIKE ?", "%#{keyword}%").order(created_at: :asc).page(params[:page])
+    elsif order_by == "BOOKMARK"
+      @reviews = Review.includes(:bookmarks).sort {|a,b| b.bookmarks.size <=> a.bookmarks.size}.page(params[:page])
+    elsif order_by == "COMMENT"
+      @reviews = Review.includes(:comments).sort {|a,b| b.comments.size <=> a.comments.size}.page(params[:page])
+    else
+      @reviews = Review.where("title LIKE ?", "%#{keyword}%").order(created_at: :desc).page(params[:page])
+    end
+    #@reviews = Review.all
   end
 
   def show
