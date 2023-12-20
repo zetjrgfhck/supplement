0a6a093023cc6d112b68b030332955bf207f0dbc
HIDEKAZU SHINOHARA <141484383+zetjrgfhck@users.noreply.github.com>
2023-12-20 10:57:39 +0000

    a

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,7 +1,7 @@
 class Admin::HomesController < ApplicationController
-  
+
   def top
     @reviews = Review.order(id: :desc).limit(4)
-    @new_comment_reviews = Review.joins(:comments).order("comments.id", :desc).limit(4)
+    @new_comment_reviews = Review.joins(:comments).order("comments.id desc").limit(4)
   end
 end
